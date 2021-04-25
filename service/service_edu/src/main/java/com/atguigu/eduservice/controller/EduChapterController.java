02f656bcfd866dcaf81c308992fde6e3dd827b0f
王冰炜 <619445114@qq.com>
2021-04-25 23:10:08 +0800

    feat: initialize the interface to get a list of course chapters

--- service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduChapterController.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduChapterController.java
@@ -1,9 +1,20 @@
 package com.atguigu.eduservice.controller;
 
 
+import com.atguigu.commonutils.R;
+import com.atguigu.eduservice.entity.chapter.ChapterVo;
+import com.atguigu.eduservice.service.EduChapterService;
+import com.atguigu.eduservice.service.EduCourseService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,9 +24,31 @@ import org.springframework.web.bind.annotation.RestController;
  * @author 王冰炜
  * @since 2021-04-22
  */
+
+@Api(tags = "章节管理")
 @RestController
 @RequestMapping("/eduservice/edu-chapter")
 public class EduChapterController {
+
+    @Autowired
+    private EduChapterService chapterService;
+
+    // 获得课程大纲列表
+    @ApiOperation(value = "所有章节列表")
+    @GetMapping("getChapterVideo/{courseId}")
+    public R getChapterVideo(@PathVariable String courseId){
+        List<ChapterVo> list = chapterService.getChapterVideoById(courseId);
+        return R.ok().data("allChapterVideo",list);
+    }
+
+    /**
+     * @ApiOperation(value = "所有讲师列表")
+     *     @GetMapping("findAll")
+     *     public R findAllTeacher(){
+     *         List<EduTeacher> list = eduTeacherService.list(null);
+     *         return R.ok().data("items",list);
+     *     }
+     */
 
 }
 
--- service/service_edu/src/main/java/com/atguigu/eduservice/service/impl/EduChapterServiceImpl.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/service/impl/EduChapterServiceImpl.java
@@ -1,10 +1,13 @@
 package com.atguigu.eduservice.service.impl;
 
 import com.atguigu.eduservice.entity.EduChapter;
+import com.atguigu.eduservice.entity.chapter.ChapterVo;
 import com.atguigu.eduservice.mapper.EduChapterMapper;
 import com.atguigu.eduservice.service.EduChapterService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +20,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class EduChapterServiceImpl extends ServiceImpl<EduChapterMapper, EduChapter> implements EduChapterService {
 
+    // 获取课程章节列表（根据课程id查询）
+    @Override
+    public List<ChapterVo> getChapterVideoById(String courseId) {
+        // 1.根据id查chapter章节
+        // 2.根据id查video小节
+        return null;
+    }
 }
