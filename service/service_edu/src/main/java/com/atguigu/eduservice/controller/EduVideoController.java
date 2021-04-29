535ccb4bb2b7c8eab5307a7e488d099f3b4d2c1c
王冰炜 <619445114@qq.com>
2021-04-29 17:34:08 +0800

    feat: realize video interface 'add' and 'delete'

--- service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduVideoController.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduVideoController.java
@@ -1,9 +1,11 @@
 package com.atguigu.eduservice.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import com.atguigu.commonutils.R;
+import com.atguigu.eduservice.entity.EduVideo;
+import com.atguigu.eduservice.service.EduVideoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -13,9 +15,39 @@ import org.springframework.web.bind.annotation.RestController;
  * @author 王冰炜
  * @since 2021-04-22
  */
+
 @RestController
-@RequestMapping("/eduservice/edu-video")
+@RequestMapping("/eduservice/video")
+@CrossOrigin
 public class EduVideoController {
+
+    @Autowired
+    EduVideoService videoService;
+
+    // 添加小节
+    @PostMapping("addVideo")
+    public R addVideo(@RequestBody EduVideo eduVideo){
+        boolean save = videoService.save(eduVideo);
+        if (save){
+            return R.ok();
+        }else {
+            return R.error();
+        }
+    }
+
+    // 删除小节
+    // TODO 删除小节时要连带删除对应的视频
+    @DeleteMapping("deleteVideo/{id}")
+    public R deleteVideo(@PathVariable String id){
+        boolean result = videoService.removeById(id);
+        if (result){
+            return R.ok();
+        }else {
+            return R.error();
+        }
+    }
+
+    // 修改小节
 
 }
 
