4baf4bb6afd6d62aea75dd0030c97ada9b55bf2e
王冰炜 <619445114@qq.com>
2021-05-09 06:30:50 +0800

    feat: complete 'send code through phone number' method, put code into redis

--- service/service_msm/src/main/java/com/atguigu/msmservice/controller/MsmApiController.java
+++ service/service_msm/src/main/java/com/atguigu/msmservice/controller/MsmApiController.java
@@ -4,10 +4,13 @@ import com.atguigu.commonutils.R;
 import com.atguigu.msmservice.service.MsmService;
 import com.atguigu.msmservice.utils.RandomUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author 王冰炜
@@ -20,16 +23,25 @@ import java.util.Map;
 public class MsmApiController {
     @Autowired
     private MsmService msmService;
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
 
     // 发送短信
     @GetMapping("send/{phone}")
     public R code(@PathVariable String phone){
-        String code = RandomUtil.getFourBitRandom();
+        // 1.从redis获取验证码，如果获取到直接返回
+        String code = redisTemplate.opsForValue().get(phone);
+        if(!StringUtils.isEmpty(code)) return R.ok();
+
+        // 2.如果redis获取不到，再进行阿里云的发送
+        code = RandomUtil.getFourBitRandom();
         Map<String, Object> param = new HashMap<>();
         param.put("code",code);
 
         boolean isSend = msmService.send(param,phone);
         if (isSend) {
+            // 发送成功，把发送成功的验证码存入redis
+            redisTemplate.opsForValue().set(phone,code,5, TimeUnit.MINUTES); // 设置有效时长
             return R.ok();
         } else {
             return R.error().message("短信发送失败！");
--- service/service_msm/src/main/java/com/atguigu/msmservice/service/impl/MsmServiceImpl.java
+++ service/service_msm/src/main/java/com/atguigu/msmservice/service/impl/MsmServiceImpl.java
@@ -6,7 +6,6 @@ import com.aliyuncs.CommonResponse;
 import com.aliyuncs.DefaultAcsClient;
 import com.aliyuncs.IAcsClient;
 import com.aliyuncs.exceptions.ClientException;
-import com.aliyuncs.exceptions.ServerException;
 import com.aliyuncs.http.MethodType;
 import com.aliyuncs.profile.DefaultProfile;
 import com.atguigu.msmservice.service.MsmService;
