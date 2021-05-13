714dc6d94fe793c1f211b35290bd7ddedee398bb
王冰炜 <619445114@qq.com>
2021-05-13 10:29:26 +0800

    chore: add WxApiController and test scan the code to log in

---
+++ service/service_ucenter/src/main/java/com/atguigu/educenter/controller/WxApiController.java
@@ -0,0 +1,72 @@
+package com.atguigu.educenter.controller;
+
+import com.atguigu.educenter.utils.ConstantPropertiesUtil;
+import com.atguigu.servicebase.exceptionhandler.GuliException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+/**
+ * @author 王冰炜
+ * @create 2021-05-12 23:37
+ */
+
+@Controller // 注意这里没有配置 @RestController，因为只是请求地址，不需要返回数据
+@RequestMapping("/api/ucenter/wx")
+@CrossOrigin
+public class WxApiController {
+    @GetMapping("login")
+    public String genQrConnect(HttpSession session) {
+
+        // 微信开放平台授权baseUrl
+        String baseUrl = "https://open.weixin.qq.com/connect/qrconnect" +
+                "?appid=%s" +
+                "&redirect_uri=%s" +
+                "&response_type=code" +
+                "&scope=snsapi_login" +
+                "&state=%s" +
+                "#wechat_redirect";
+
+        // 回调地址
+        String redirectUrl = ConstantPropertiesUtil.WX_OPEN_REDIRECT_URL; //获取业务服务器重定向地址
+        try {
+            redirectUrl = URLEncoder.encode(redirectUrl, "UTF-8"); //url编码
+        } catch (UnsupportedEncodingException e) {
+            throw new GuliException(20001, e.getMessage());
+        }
+
+        // 防止csrf攻击（跨站请求伪造攻击）
+        //String state = UUID.randomUUID().toString().replaceAll("-", "");//一般情况下会使用一个随机数
+        String state = "imhelen";//为了让大家能够使用我搭建的外网的微信回调跳转服务器，这里填写你在ngrok的前置域名
+        System.out.println("state = " + state);
+
+        // 采用redis等进行缓存state 使用sessionId为key 30分钟后过期，可配置
+        //键："wechar-open-state-" + httpServletRequest.getSession().getId()
+        //值：satte
+        //过期时间：30分钟
+
+        //生成qrcodeUrl
+        String qrcodeUrl = String.format(
+                baseUrl,
+                ConstantPropertiesUtil.WX_OPEN_APP_ID,
+                redirectUrl,
+                state);
+
+        return "redirect:" + qrcodeUrl;
+    }
+
+    // 获取扫描人信息，添加数据
+    @GetMapping("callback")
+    public String callback(String code, String state, HttpSession session) {
+
+        //得到授权临时票据code
+        System.out.println("code = " + code);
+        System.out.println("state = " + state);
+        return "redirect:http://localhost:3000";
+    }
+}
